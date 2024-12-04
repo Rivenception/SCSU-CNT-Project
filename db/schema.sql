3297091a2ac9c7904daee881c9ff1bc88d0d258c
Rivenception <lootz.dev@gmail.com>
2024-12-04 13:24:36 -0500

    added to the api selects list

--- db/schema.sql
+++ db/schema.sql
@@ -1,152 +1,33 @@
-DROP DATABASE IF EXISTS time_monitorDB;
- 
-CREATE DATABASE time_monitorDB;
- 
-USE time_monitorDB;
- 
-CREATE TABLE students (
-  student_id varchar(255) NOT NULL,
-  studentName varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  position varchar(255) NOT NULL,
-  supervisor varchar(255) NOT NULL,
-  status varchar(255) NOT NULL DEFAULT 'Active',
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (studentName)
-);
- 
-  -- ^ jay
- 
-CREATE TABLE cnttimesheets (
-  timesheet_id int NOT NULL AUTO_INCREMENT,
-  studentName varchar(255) NOT NULL,
-  date date NOT NULL,
-  project_id varchar(255) NOT NULL DEFAULT 'Admin',
-  category varchar(255) NOT NULL DEFAULT 'Daily Log',
-  logNotes text NOT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (timesheet_id),
-  FOREIGN KEY (project_id) REFERENCES project(project_id)
-);
- 
-  -- ^ jay
- 
-CREATE TABLE clockings (
-  clock_id int NOT NULL AUTO_INCREMENT,
-  student_id varchar(255) NOT NULL,
-  date date NOT NULL,
-  timeType enum('Time In','Time Out') NOT NULL,
-  timeEntry time NOT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (clock_id)
-  FOREIGN KEY (studentName) REFERENCES students(studentName)
-);
- 
-  -- ^ jay
- 
-CREATE TABLE faculty (
-  faculty_id NOT NULL,
-  facultyName varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  dept varchar(255) NOT NULL,
-  title varchar(255) NOT NULL,
-  manager varchar(255) DEFAULT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (facultyName)
-);
+DROP DATABASE time_monitorDB_test;
 
-  -- ^ jay
- 
-CREATE TABLE project (
-  project_id NOT NULL AUTO_INCREMENT,
-  projectName varchar(255) NOT NULL,
-  projectSponsor varchar(255) NOT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (projectName)
-  FOREIGN KEY (affiliateID) REFERENCES affiliate(affiliateID)
-);
- 
-  -- ^ josh
- 
-CREATE TABLE affiliateContact (
-  person_id NOT NULL,
-  personName varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (personName)
-  FOREIGN KEY (projectName) REFERENCES project(projectName)
-  FOREIGN KEY (affiliateID) REFERENCES affiliate(affiliateID)
-);
- 
--- ^ josh
- 
-CREATE TABLE affiliate (
-  affiliate_id NOT NULL AUTO_INCREMENT,
-  personName varchar(255) NOT NULL,
-  projectAffiliation varchar(255) NOT NULL,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (affiliateName)
-  FOREIGN KEY (projectSponsor) REFERENCES project(projectSponsor)
-  );
- 
--- ^ josh
- 
-CREATE TABLE transactions (
-  transaction_id NOT NULL AUTO_INCREMENT,
-  personName varchar(255) NOT NULL,
-  projectAffiliation varchar(255) NOT NULL,
-  lastPayment datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  nextPayment datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  paymentStatus DECIMAL(10, 2) NOT NULL,
-  paymentAmount DECIMAL (10,2) NOT NULL,
-  PRIMARY KEY (transaction_id)
-  FOREIGN KEY (affiliateName) REFERENCES affiliate(affiliateName) ON DELETE CASCADE
-);
- 
-  -- ^ josh
+CREATE DATABASE time_monitorDB_test;
 
--- Revised Tables Section: Rick  **double check on delete relations**
+USE time_monitorDB_test;
 
 CREATE TABLE affiliate (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     affiliateName VARCHAR(255) NOT NULL PRIMARY KEY,
     projectAffiliation VARCHAR(255) NOT NULL,
     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE projectAffiliate (
-    affiliateName VARCHAR(255) NOT NULL,
-    projectSponsor VARCHAR(255) NOT NULL,
-    PRIMARY KEY (affiliateName, projectSponsor),
-    FOREIGN KEY (affiliateName) REFERENCES Affiliate(affiliateName) ON DELETE CASCADE,
-    FOREIGN KEY (projectSponsor) REFERENCES Project(projectSponsor) ON DELETE CASCADE
-);
-
-CREATE TABLE transactions (
+CREATE TABLE transaction (
   transaction_id INT AUTO_INCREMENT,
   affiliateName varchar(255) NOT NULL,
   projectAffiliation varchar(255) NOT NULL, -- this might need to be a FK.
-  paymentStatus varchar(255) NOT NULL,
-  paymentAmount DECIMAL (10,2) NOT NULL,
-  lastPayment datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  nextPayment datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  paymentStatus varchar(255) DEFAULT 'Pending',
+  paymentAmount DECIMAL (10,2),
+  lastPayment date,
+  nextPayment date,
   createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (transaction_id),
-  FOREIGN KEY (affiliateName) REFERENCES affiliate(affiliateName) ON DELETE CASCADE
+  FOREIGN KEY (affiliateName) REFERENCES affiliate(affiliateName) ON DELETE NO ACTION
 );
 
 CREATE TABLE affiliateContact (
-  person_id NOT NULL,
+  person_id INT AUTO_INCREMENT UNIQUE NOT NULL,
   personName varchar(255), -- remove the NOT NULL because a contact could just be "billing@example.com" for the address with no one specific.
   affiliateName varchar(255),
   email varchar(255) NOT NULL,
@@ -157,17 +38,50 @@ CREATE TABLE affiliateContact (
 );
 
 CREATE TABLE project (
-  project_id INT AUTO_INCREMENT,
-  projectName varchar(255) NOT NULL,
+  project_id INT AUTO_INCREMENT UNIQUE,
+  projectName varchar(255) NOT NULL UNIQUE,
   projectSponsor varchar(255), -- can be NULL. not all projects have sponsors
+  status ENUM('Active', 'Inactive') NOT NULL DEFAULT 'Active',
   createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (projectName),
-  FOREIGN KEY (projectSponsor) REFERENCES affiliate(affiliateName) -- no on delete here since a sponsor can stop funding a project at any time
+  PRIMARY KEY (project_id),
+  FOREIGN KEY (projectSponsor) REFERENCES affiliate(affiliateName) ON DELETE SET NULL -- on delete remove the sponsor and set to NULL
 );
 
-CREATE TABLE Task (
-    task_id INT NOT NULL PRIMARY KEY,
+CREATE TABLE projectAffiliate (
+    affiliateName VARCHAR(255) NOT NULL,
+    projectSponsor VARCHAR(255) NOT NULL,
+    PRIMARY KEY (affiliateName, projectSponsor),
+    FOREIGN KEY (affiliateName) REFERENCES Affiliate(affiliateName),
+    FOREIGN KEY (projectSponsor) REFERENCES Project(projectSponsor)
+);
+
+CREATE TABLE student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(255) NOT NULL UNIQUE,
+    studentName VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    supervisor VARCHAR(255),
+    status ENUM('Active', 'Inactive') NOT NULL DEFAULT 'Active',
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE faculty (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    facultyName VARCHAR(255) NOT NULL UNIQUE,
+    faculty_id VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    dept VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    manager VARCHAR(255),
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE task (
+    task_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     projectName VARCHAR(255) NOT NULL,
     task VARCHAR(255) NOT NULL,
     taskNotes VARCHAR(255),
@@ -178,7 +92,8 @@ CREATE TABLE Task (
     status VARCHAR(255),
     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (projectName) REFERENCES Project(projectName) ON DELETE CASCADE
+    FOREIGN KEY (projectName) REFERENCES Project(projectName) ON DELETE NO ACTION,
+    FOREIGN KEY (assignedTo) REFERENCES Student(studentName) ON DELETE NO ACTION
 );
 
 CREATE TABLE cntTimesheet (
@@ -194,7 +109,7 @@ CREATE TABLE cntTimesheet (
     FOREIGN KEY (projectName) REFERENCES Project(projectName) ON DELETE CASCADE
 );
 
-CREATE TABLE Clocking (
+CREATE TABLE clocking (
     clock_id INT AUTO_INCREMENT PRIMARY KEY,
     studentName VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
@@ -203,26 +118,4 @@ CREATE TABLE Clocking (
     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (studentName) REFERENCES Student(studentName) ON DELETE CASCADE
-);
-
-CREATE TABLE Student (
-    student_id VARCHAR(255) NOT NULL UNIQUE,
-    studentName VARCHAR(255) NOT NULL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL,
-    supervisor VARCHAR(255),
-    status ENUM('Active', 'Inactive') NOT NULL DEFAULT 'Active',
-    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Faculty (
-    facultyName VARCHAR(255) NOT NULL PRIMARY KEY,
-    faculty_id VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    dept VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    manager VARCHAR(255),
-    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
